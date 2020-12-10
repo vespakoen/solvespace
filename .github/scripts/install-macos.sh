d07b98ef40c61e6ff6448813d07bc3d8ece5f9c0
Koen Schmeets <hello@koenschmeets.nl>
2020-12-10 21:15:41 +0100

    Apple Silicon build

--- .github/scripts/install-macos.sh
+++ .github/scripts/install-macos.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -xe
 
-brew install libomp
+if [ "$1" == "silicon" ]; then
+    export ARCHFLAGS='-arch arm64'
+    brew install --verbose --build-from-source libomp zlib cairo pixman
+    brew reinstall --verbose --build-from-source --HEAD libpng freetype
+else
+    brew install libomp libpng zlib freetype
+fi
 git submodule update --init
